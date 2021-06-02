a10b2b2a835a9b1046f74c8174bd4d911bc524ba
balao1312 <balao1014@hotmail.com>
2021-06-02 12:39:00 +0800

    add some basic essentials

---
+++ main.py
@@ -0,0 +1,12 @@
+import re
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+def main():
+    return {'host': 'on'}
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
