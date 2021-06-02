d716676ad319063d4ab12162aee0842fdedc8d43
balao1312 <balao1014@hotmail.com>
2021-06-02 12:46:55 +0800

    add balao function

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 from fastapi import FastAPI
 import uvicorn
 
@@ -7,6 +6,17 @@ app = FastAPI()
 @app.get('/')
 def main():
     return {'host': 'on'}
+
+@app.get('/balao')
+def balao():
+    return {'data': 
+        {
+            'name': 'balao',
+            'age': '38',
+            'gender': 'male',
+            'profession': 'ToDiGong'
+        }
+    }
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
