21df80e2cc924bf13983b944baa40f110d81b119
eric_li@mail.chungyo.net <eric_li@mail.chungyo.net>
2021-06-03 12:33:36 +0800

    新增AV男演,修改requirements

--- main.py
+++ main.py
@@ -6,35 +6,51 @@ from database.database import db_con
 
 app = FastAPI()
 
+
 @app.get('/')
 def main():
     return {'host': 'on'}
 
+
 @app.get('/balao')
 def balao():
-    return {'data': 
-        {
-            'name': 'balao',
-            'age': '38',
-            'gender': 'male',
-            'profession': 'ToDiGong'
-        }
-    }
+    return {'data':
+            {
+                'name': 'balao',
+                'age': '38',
+                'gender': 'male',
+                'profession': 'ToDiGong'
+            }
+            }
+
+
+@app.get('/zong_yu')
+def zong_yu():
+    return {'data':
+            {
+                'name': 'zong_yu',
+                'age': '30',
+                'gender': 'male',
+                'profession': 'av actor'
+            }
+            }
+
 
 @app.get('/blog')
 def show_all_blogs():
     data = db_con.show_all_blogs()
     return {'data': data}
 
+
 @app.post('/blog')
 def create_blog(blog: Blog):
     db_con.create_new_blog(
-        author = blog.author,
-        title = blog.title,
-        content = blog.content,
-        m_time = blog.m_time,
-        comments = blog.comments,
-        likes = blog.likes
+        author=blog.author,
+        title=blog.title,
+        content=blog.content,
+        m_time=blog.m_time,
+        comments=blog.comments,
+        likes=blog.likes
     )
     return {
         'status': 'blog created successfully.',
@@ -42,6 +58,8 @@ def create_blog(blog: Blog):
     }
 
 # Will RESET table blogs!! Use with care!!
+
+
 @app.get('/reset_table_blogs')
 def reset_table_blogs():
     db_con.reset_table_blogs()
