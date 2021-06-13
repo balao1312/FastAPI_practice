03686e13cb764c25cd172b3d0f9c5153987b8247
balao1312 <balao1014@hotmail.com>
2021-06-13 08:19:58 +0800

    restructure with app folder & module rename

---
+++ application/apis/main.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter
+from ..database import db_con
+from ..models.blogs import Blog
+
+router = APIRouter()
+
+
+@router.get('/')
+def main():
+    return {'host': 'on'}
+
+@router.get('/blog')
+def show_all_blogs():
+    data = db_con.show_all_blogs()
+    return {'data': data}
+
+
+@router.get('/blog/{pk}')
+def get_blog_by_pk(pk):
+    data = db_con.get_blog_by_pk(pk)
+    if 'error' in data:
+        return data
+    return {'data': data}
+
+
+@router.post('/blog')
+def create_blog(blog: Blog):
+    db_con.create_new_blog(
+        author=blog.author,
+        title=blog.title,
+        content=blog.content,
+        m_time=blog.m_time,
+        comments=blog.comments,
+        likes=blog.likes
+    )
+    return {
+        'status': 'blog created successfully.',
+        'data': blog
+    }
+
+
+@router.delete('/blog/{pk}')
+def delete_blog_by_pk(pk):
+    data = db_con.delete_blog_by_pk(pk)
+    if 'error' in data:
+        return data
+    return {'status': f'blog with pk = {pk} deleted successfully'}
+
+
+@router.get('/reset_table_blogs')
+def reset_table_blogs():
+    db_con.reset_table_blogs()
+    return {'status': 'reset table blogs successfully.'}
---
+++ application/database.py
@@ -0,0 +1,88 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from credential import db_info
+
+
+class PostgreSQL_connection():
+    def __init__(self, host, database, user, password, port=5432):
+        self.con = psycopg2.connect(
+            host=db_info['host'],
+            user=db_info['user'],
+            database=db_info['database'],
+            password=db_info['password'],
+            port=db_info['port']
+        )
+        self.cursor = self.con.cursor(cursor_factory=RealDictCursor)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.con.close()
+
+    def show_all_databases(self):
+        self.cursor.execute('SELECT datname FROM pg_database;')
+        return self.cursor.fetchall()
+
+    def get_blog_by_pk(self, pk):
+        if not pk.isdigit():
+            return {'error': 'pk is not digit.'}
+
+        self.cursor.execute(f"SELECT * from blogs where pk = '{pk}'")
+
+        result = self.cursor.fetchall()
+        if not result:
+            return {'error': 'target is not exist.'}
+
+        return result
+
+    def delete_blog_by_pk(self, pk):
+        if not pk.isdigit():
+            return {'error': 'pk is not digit.'}
+
+        self.cursor.execute(f"SELECT * from blogs where pk = '{pk}'")
+        result = self.cursor.fetchall()
+        if not result:
+            return {'error': 'target is not exist'}
+
+        self.cursor.execute(f"delete from blogs where pk = '{pk}'")
+        self.con.commit()
+        return {'status': 'success'}
+
+    def show_all_blogs(self):
+        self.cursor.execute('SELECT * from blogs;')
+        result = self.cursor.fetchall()
+        return result
+
+    def create_new_blog(self, author, title, content, m_time, comments, likes):
+        sql = f'''INSERT INTO blogs (author, title, content, m_time, comments, likes)
+        VALUES ('{author}', '{title}', '{content}', '{m_time}', '{comments}', {likes});'''
+        self.cursor.execute(sql)
+        self.con.commit()
+        return {'status': 'success'}
+
+    # Will RESET table blogs!! Use with care!!
+    def reset_table_blogs(self):
+        sql = f'''drop table if exists blogs;
+            drop sequence if exists blogs_pk_seq;
+            create sequence blogs_pk_seq;
+            create table blogs (
+            pk integer not null default nextval('blogs_pk_seq'::regclass),
+            author text not null,
+            title text not null,
+            content text COLLATE pg_catalog."default" NOT NULL,
+            m_time timestamp not null,
+            comments jsonb,
+            likes integer,
+            CONSTRAINT blogs_pkey PRIMARY KEY (pk)
+            )
+            tablespace pg_default;'''
+        self.cursor.execute(sql)
+        self.con.commit()
+        return {'status': 'success'}
+
+
+db_con = PostgreSQL_connection(
+    host=db_info['host'],
+    user=db_info['user'],
+    database=db_info['database'],
+    password=db_info['password'],
+    port=db_info['port']
+)
